3d25c0474b44ec1571f637ce83308cbac8a9d26c
Leonardo Braga <leonardobraga24@gmail.com>
2019-09-17 15:12:07 -0300

    Trocando a Tela2 para uma função
    Também corrigida a acentuação

--- src/Tela2.js
+++ src/Tela2.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 
@@ -9,22 +8,20 @@ class MyButton extends Component {
     }
 }
 
-class Tela2 extends Component {
-    render() {
+function Tela2() {
         return (
             <div className="App">
                 <header className="App-header">
                     <h1>Seja Bem Vindo Cliente</h1>
-                    <p>Já e Cadastrado? <MyButton button="Login"/></p>
+                    <p>JÃ¡ Ã© Cadastrado? <MyButton button="Login"/></p>
                     <p>Cansado de esperar na fila de atendimento ou no caixa?</p>
-                    <p>Acordou atrasado para ir ao trabalho e não tem tempe de tomar um café, mas consegue ir a padaria?</p>
-                    <p>Quer ganhar um tempinho antes daquela aula e ver aquele video que você deixou pra mais tarde?</p>
-                    <p>Pois saiba que com o App Bake Fast você consegue fazer tudo isso! </p>
+                    <p>Acordou atrasado para ir ao trabalho e nÃ£o tem tempe de tomar um cafÃ©, mas consegue ir a padaria?</p>
+                    <p>Quer ganhar um tempinho antes daquela aula e ver aquele video que vocÃª deixou pra mais tarde?</p>
+                    <p>Pois saiba que com o App Bake Fast vocÃª consegue fazer tudo isso! </p>
                     <MyButton button="Como Usar" />
                 </header>
             </div>
         );
-    }
 }
 
 export default Tela2;
